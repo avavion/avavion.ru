c76e9656760e6062b2839659149b1561535735d9
Alan Bogov (avavion) <avavionmvm@gmail.com>
2023-05-22 21:09:22 +0300

    Add langs

---
+++ src/hooks/useLang.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+
+const useLang = () => {
+  const [lang, setLang] = useState("en");
+
+  useEffect(() => {}, []);
+};
+
+export default useLang;
---
+++ src/serivces/Storage.js
@@ -0,0 +1,23 @@
+class Storage {
+  static get(key) {
+    if (this.isNull(key)) {
+      return {};
+    }
+
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  static set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  static isNull(key) {
+    if (localStorage.getItem(key) === null) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
+export default Storage;
